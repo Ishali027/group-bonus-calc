bf60c50c6edf5a8372255b8be8363d6039524c15
Ismail Ali <ishaliprime@gmail.com>
2023-05-03 00:43:26 -0500

    wrote a starter function and for loop, was unable to complete before live code

--- client.js
+++ client.js
@@ -47,11 +47,64 @@ console.log('array of employee data: ',  employees );
 
 
 // This function will calculate 1 employee's bonus!
+
+/*
+
+Input:
+  {
+    name: 'Mayella',
+    employeeNumber: '89068',
+    annualSalary: '35000',
+    reviewRating: 1
+  }
+*/
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
-  
-  
+
+
+    if (employee.reviewRating <= 2) {
+      bonusPercentage = 0
+    }
+    else if( employee.reviewRating === 3){
+      bonusPercentage = .04;
+    }
+    else if( employee.reviewRating === 4){
+      bonusPercentage = .06;
+    }
+    else if( employee.reviewRating === 5){
+      bonusPercentage = .1;
+    }
+
+    else if( employee.employeeNumber.length === 4){
+      bonusPercentage += 0.05;
+    }
+   
+    else if( employee.annualSalary > 65,000){
+      bonusPercentage -= 0.01;
+    }
+   
   // return new object with bonus results
 
+
+
+
+
+    return
+  const employeeBonus = {
+    name: employee.name,
+    bonusPercentage: 0,
+    totalCompensation: 0,
+    totalBonus: 0
+
+    
+  }
+  
+
+  }
+
+
+
+for(let i = 0; i<employees.length; i++){
+    calculateIndividualEmployeeBonus(employees[i])
 }
